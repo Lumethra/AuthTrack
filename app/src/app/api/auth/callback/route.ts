26ab3fd3a1e9ce4a12fa559064f036b95e8c28e3
Maximilian Zhu <maximilian.zhu@loewenrot.de>
2025-08-01 18:37:03 +0200

    nah, the doc was trash, returned to working version without redirect and then made redirect with native and not NextRequest

--- app/src/app/api/auth/callback/route.ts
+++ app/src/app/api/auth/callback/route.ts
@@ -1,26 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-    const code = req.nextUrl.searchParams.get("code");
-
-    if (!code) {
-        return NextResponse.redirect("/error");
-    }
+export async function GET(req: Request) {
+    const { searchParams } = new URL(req.url);
+    const code = searchParams.get("code");
 
     const response = await fetch("https://slack.com/api/oauth.v2.access", {
         method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams({
-            code,
             client_id: process.env.SLACK_CLIENT_ID!,
             client_secret: process.env.SLACK_CLIENT_SECRET!,
+            code: code ?? "",
             redirect_uri: process.env.SLACK_REDIRECT_URI!,
         }),
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
     });
 
     const data = await response.json();
 
     console.log(JSON.stringify(data, null, 2));
 
-    return NextResponse.redirect("/dashboard");
+    return Response.redirect(new URL("/dashboard", req.url));
 }
