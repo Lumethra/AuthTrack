87e79b9a4fbd17fa67de4032e0f1d287e4b289a5
Maximilian Zhu <maximilian.zhu@loewenrot.de>
2025-08-01 17:53:03 +0200

    fixing invalid scopes with adding scopes of the manifest.json

--- app/src/app/api/auth/slack/route.ts
+++ app/src/app/api/auth/slack/route.ts
@@ -1,6 +1,15 @@
 import { redirect } from "next/navigation";
 
 export async function GET() {
-    const slackAuthUrl = `https://slack.com/oauth/v2/authorize?client_id=${process.env.SLACK_CLIENT_ID}&scope=identity.basic&redirect_uri=${process.env.SLACK_REDIRECT_URI}`;
+    const scopes = [
+        "users:read",
+        "channels:history",
+        "groups:history",
+        "im:history",
+        "mpim:history"
+    ].join(",");
+
+    const slackAuthUrl = `https://slack.com/oauth/v2/authorize?client_id=${process.env.SLACK_CLIENT_ID}&scope=${scopes}&redirect_uri=${process.env.SLACK_REDIRECT_URI}`;
+
     redirect(slackAuthUrl);
 }
